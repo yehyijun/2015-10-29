45f45f85f6600846d353e166d6ffc91df7bbabdb
yehyijun <yehyijun@gmail.com>
2015-10-29 00:58:38 +0800

    123

---
+++ WindowsApplication2/Form1.Designer.vb
@@ -0,0 +1,113 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 覆寫 Dispose 以清除元件清單。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    '為 Windows Form 設計工具的必要項
+    Private components As System.ComponentModel.IContainer
+
+    '注意:  以下為 Windows Form 設計工具所需的程序
+    '可以使用 Windows Form 設計工具進行修改。
+    '請不要使用程式碼編輯器進行修改。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnlg = New System.Windows.Forms.Button()
+        Me.btnexit = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtusername = New System.Windows.Forms.TextBox()
+        Me.txtpassword = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'btnlg
+        '
+        Me.btnlg.Font = New System.Drawing.Font("新細明體", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.btnlg.Location = New System.Drawing.Point(37, 197)
+        Me.btnlg.Name = "btnlg"
+        Me.btnlg.Size = New System.Drawing.Size(75, 23)
+        Me.btnlg.TabIndex = 0
+        Me.btnlg.Text = "登入"
+        Me.btnlg.UseVisualStyleBackColor = True
+        '
+        'btnexit
+        '
+        Me.btnexit.Font = New System.Drawing.Font("新細明體", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.btnexit.Location = New System.Drawing.Point(151, 197)
+        Me.btnexit.Name = "btnexit"
+        Me.btnexit.Size = New System.Drawing.Size(75, 23)
+        Me.btnexit.TabIndex = 1
+        Me.btnexit.Text = "離開"
+        Me.btnexit.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("新細明體", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Label1.Location = New System.Drawing.Point(45, 70)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(40, 16)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "帳號"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("新細明體", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Label2.Location = New System.Drawing.Point(45, 128)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(40, 16)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "密碼"
+        '
+        'txtusername
+        '
+        Me.txtusername.Font = New System.Drawing.Font("新細明體", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.txtusername.Location = New System.Drawing.Point(136, 67)
+        Me.txtusername.Name = "txtusername"
+        Me.txtusername.Size = New System.Drawing.Size(100, 27)
+        Me.txtusername.TabIndex = 4
+        '
+        'txtpassword
+        '
+        Me.txtpassword.Font = New System.Drawing.Font("新細明體", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.txtpassword.Location = New System.Drawing.Point(136, 125)
+        Me.txtpassword.Name = "txtpassword"
+        Me.txtpassword.Size = New System.Drawing.Size(100, 27)
+        Me.txtpassword.TabIndex = 5
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.Controls.Add(Me.txtpassword)
+        Me.Controls.Add(Me.txtusername)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnexit)
+        Me.Controls.Add(Me.btnlg)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnlg As System.Windows.Forms.Button
+    Friend WithEvents btnexit As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents txtusername As System.Windows.Forms.TextBox
+    Friend WithEvents txtpassword As System.Windows.Forms.TextBox
+
+End Class
---
+++ WindowsApplication2/Form1.vb
@@ -0,0 +1,34 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+    Private Sub btnlg_click(sender As Object, e As EventArgs) Handles btnlg.Click
+        If txtusername.Text = "" And txtpassword.Text = "" Then
+            txtusername.BackColor = Color.MistyRose
+            txtpassword.BackColor = Color.MistyRose
+        End If
+
+        If txtusername.Text = "login" Then
+            txtusername.BackColor = Color.White
+        Else
+            txtusername.BackColor = Color.MistyRose
+        End If
+
+        If txtpassword.Text = "abc123" Then
+            txtpassword.BackColor = Color.White
+        Else
+            txtpassword.BackColor = Color.MistyRose
+        End If
+
+        If txtusername.Text = "login" And txtpassword.Text = "abc123" Then
+            MessageBox.Show("帳號密碼正確")
+            Application.Exit()
+        End If
+    End Sub
+
+    Private Sub btnexit_click(sender As Object, e As EventArgs) Handles btnexit.Click
+        Application.Exit()
+    End Sub
+
+End Class
